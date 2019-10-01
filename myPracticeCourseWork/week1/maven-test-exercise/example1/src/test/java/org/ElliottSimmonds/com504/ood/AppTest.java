4d53e393e31f5adbede32df39e6cfdda8cf66dda
ElliottSimmonds <simmonds.elliott@yahoo.co.uk>
2019-10-01 23:14:06 +0100

    Completed task 2

---
+++ myPracticeCourseWork/week1/maven-test-exercise/example1/src/test/java/org/ElliottSimmonds/com504/ood/AppTest.java
@@ -0,0 +1,21 @@
+package org.ElliottSimmonds.com504.ood;
+
+import MyTestClassLog4j;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( MyTestClassLog4j.writeAboutMe() );
+    }
+}
