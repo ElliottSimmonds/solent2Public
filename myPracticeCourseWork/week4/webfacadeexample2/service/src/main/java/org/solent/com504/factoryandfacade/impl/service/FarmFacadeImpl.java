51fb0b44c305ac4457c1a8013f6b610ba3a143f9
Elliott Simmonds <4simme28@solent.ac.uk>
2019-10-30 11:35:18 +0000

    Implementing methods

--- myPracticeCourseWork/week4/webfacadeexample2/service/src/main/java/org/solent/com504/factoryandfacade/impl/service/FarmFacadeImpl.java
+++ myPracticeCourseWork/week4/webfacadeexample2/service/src/main/java/org/solent/com504/factoryandfacade/impl/service/FarmFacadeImpl.java
@@ -1,9 +1,11 @@
 package org.solent.com504.factoryandfacade.impl.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.solent.com504.factoryandfacade.model.dao.AnimalDao;
 import org.solent.com504.factoryandfacade.model.dao.AnimalTypeDao;
 import org.solent.com504.factoryandfacade.model.dto.Animal;
+import org.solent.com504.factoryandfacade.model.dto.AnimalType;
 import org.solent.com504.factoryandfacade.model.service.FarmFacade;
 
 public class FarmFacadeImpl implements FarmFacade {
@@ -24,31 +26,45 @@ public class FarmFacadeImpl implements FarmFacade {
     // Farm facade methods
     @Override
     public List<Animal> getAllAnimals() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return animalDao.retrieveAll();
     }
 
     @Override
-    public Animal addAnimal(String animalType, String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Animal addAnimal(String animalTypeStr, String name) {
+        AnimalType animalType = animalTypeDao.getAnimalType(animalTypeStr);
+        Animal animal = animalDao.create(animalType);
+        animal.setName(name);
+        return animal;
     }
 
     @Override
-    public List<Animal> getAnimalsOfType(String animalType) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<Animal> getAnimalsOfType(String animalTypeStr) {
+        AnimalType animalType = animalTypeDao.getAnimalType(animalTypeStr);
+        Animal templateAnimal = animalDao.create(animalType);
+        List<Animal> typeList = animalDao.retrieve(templateAnimal);
+        return typeList;
     }
 
     @Override
     public Animal getAnimal(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Animal animalTemplate = new Animal();
+        animalTemplate.setName(name);
+        List<Animal> animals = animalDao.retrieve(animalTemplate);
+        return animals.get(0);
     }
 
     @Override
     public boolean removeAnimal(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Animal animal = getAnimal(name);
+        Long id = animal.getId();
+        return animalDao.delete(id);
     }
 
     @Override
     public List<String> getSupportedAnimalTypes() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<String> stringTypeList = new ArrayList<>();
+        List<AnimalType> typeList = animalTypeDao.getSupportedAnimalTypes();
+        //iterate through typelist, get type as string and append to stringtypelist
+        return stringTypeList;
     }
 }
